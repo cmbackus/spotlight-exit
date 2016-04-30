f8e72cce15c7d26bc3436384bdb0da28a17eeeff
Charlie Backus <cbackus123@gmail.com>
2016-04-30 14:32:50 -0400

    first commit

---
+++ data.js
@@ -0,0 +1,32 @@
+answers = [{
+        "user_name": "Charlie"
+        , "genres": [1, 2, null, null]
+}, {
+        "user_name": "Jason"
+        , "genres": [null, null, null, 2]
+}, {
+        "user_name": "Jon"
+        , "genres": [null, null, null, 2]
+}, {
+        "user_name": "Isaiah"
+        , "genres": [null, null, null, 2]
+}, {
+        "user_name": "Jason"
+        , "genres": [null, null, null, null]
+}, {
+        "user_name": "Jason"
+        , "genres": [null, null, null, 1]
+}, {
+        "user_name": "Jason"
+        , "genres": [null, 3, null, 4]
+}, {
+        "user_name": "Jason"
+        , "genres": [null, null, 2, null]
+}, {
+        "user_name": "Jason"
+        , "genres": [null, 1, null, null]
+}, {
+        "user_name": "Jason"
+        , "genres": [null, 1, null, 2]
+}
+          ];
